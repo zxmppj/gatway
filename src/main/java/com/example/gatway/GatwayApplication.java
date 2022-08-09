3a5381d6d68eee03f2cdb310bca84e974500f021
zhaoxiumin <zhaoxiumin@ysstech.com>
2022-08-09 23:58:12 +0800

    注册中心客户端

--- src/main/java/com/example/gatway/GatwayApplication.java
+++ src/main/java/com/example/gatway/GatwayApplication.java
@@ -2,8 +2,10 @@ package com.example.gatway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatwayApplication {
 
     public static void main(String[] args) {
